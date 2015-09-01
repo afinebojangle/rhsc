f61f464ad4c62f12ee3f50818f38f1c344da46a5
afinebojangle <afinebojangle@gmail.com>
2015-09-01 20:42:11 +0000

    created employee namespace

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,11 @@ Rails.application.routes.draw do
   get '/after' => 'welcome#after'
   
   root 'welcome#home'
+  
+  namespace :employees do
+    root 'employees#home'
+    
+  end
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
